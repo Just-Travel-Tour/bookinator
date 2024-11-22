86fce5fafddd87c3c79719cb6ca6084652a81900
Juliana <juliana.aragao@justtraveltour.com>
2024-11-22 18:46:50 -0300

    fix done usage reply function

--- src/utils/doneUsageReply.js
+++ src/utils/doneUsageReply.js
@@ -1,16 +1,20 @@
+import { embedTitlePrefix } from "../views/index.js";
+
 export async function doneUsageReply(interaction) {
-  const newStateId = interaction.customId
+  const newStateId = interaction.customId;
   const prevEmbed = interaction.message.embeds[0];
   const prevEmbedData = prevEmbed.toJSON();
-  const oldState = prevEmbedData.fields.find((field) => field.name === "Estado").value;
-  const taskCode = prevEmbedData.fields.find(field => field.name === "Codigo da atividade / Descrição").value
+  const oldState = prevEmbedData.fields.find(
+    (field) => field.name === "Estado"
+  ).value;
+  const taskCode = prevEmbedData.title.replace(embedTitlePrefix, "");
 
   if (
     oldState === "🔴 Em teste" &&
     (newStateId === "complete_test" || newStateId === "reschedule")
   ) {
     await interaction.channel.send(
-      `📢 @Tecnologia, a atividade "${taskCode}" saiu de homologação`
+      `📢 A atividade "${taskCode}" saiu de homologação`
     );
   }
 }
